fd1832ad9ad8cd540b5d087174ecc13ad014a99d
chidambaram0001 <96237777+chidambaram0001@users.noreply.github.com>
2024-05-30 11:45:12 +0530

    Create shared.reducer.ts

---
+++ src/app/sharedState/reducers/shared.reducer.ts
@@ -0,0 +1,13 @@
+import { createReducer, on } from "@ngrx/store";
+import { sharedState } from "../shared.state";
+import { updateLoginStatue } from "../action/shared.action";
+
+export function sharedReducer(state, action){
+
+    return createReducer(sharedState, on(updateLoginStatue,(state,action)=>{
+        return {...state, isLoggedIn:action.isLoggedIn}
+    }))(state,action)
+
+}
+
+
